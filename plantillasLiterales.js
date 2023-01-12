6e909de6f5b3efd7d041a40f1abd892c031162bb
Juan-Little <juanbenitodevalle@gmail.com>
2023-01-12 22:53:57 +0100

    corregido en casa refinitivo para presentar

--- plantillasLiterales.js
+++ plantillasLiterales.js
@@ -2,26 +2,30 @@
 
 //Formas de declarar un string:
 
-const nombre = 'Juan'
-const apellido = 'Benito de Valle'; //comillas simples
+//comillas simples
 
-const ciudad = "Sevilla";//comillas dobles
+const nombre = 'Juan'
+const apellido = 'Benito de Valle'; 
+
+//comillas dobles
+
+const ciudad = "Sevilla";
 
 const saludo = "hola, " + nombre + apellido + ", ¿Cómo estás?";
 
 //console.log(saludo);
 
-//con backtick (backquote, grave, or grave accent)
+//con backtick o acento grave ` (backquote, grave, or grave accent)
 
 const saludo2 = `hola, ${nombre} ${apellido}  , ¿cómo estás?`; //usar una variable en un string se llama string interpolation
 
-console.log(saludo2);
+// console.log(saludo2);
 
 
 
 descripcionPersonal =`${nombre} ${apellido} es de ${ciudad}.`
 
- console.log(descripcionPersonal)
+//  console.log(descripcionPersonal)
 
 const datosPersonalesEnTresLineas = ` Me llamo ${nombre},
  mi primer apellido es ${apellido},

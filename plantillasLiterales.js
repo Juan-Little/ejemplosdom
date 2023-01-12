ae5c15148901b03a6fdd708a23a63cabbe37fae8
Juan-Little <juanbenitodevalle@gmail.com>
2023-01-12 22:45:35 +0100

    ejemplos presentacion plantilla literal revisado con maria

--- plantillasLiterales.js
+++ plantillasLiterales.js
@@ -2,25 +2,26 @@
 
 //Formas de declarar un string:
 
-nombre = 'Juan'
-apellido = 'Benito de Valle'; //comillas simples
+let nombre = 'Juan'
+let apellido = 'Benito de Valle'; //comillas simples
 
 const ciudad = "Sevilla";//comillas dobles
 
 const saludo = "hola, " + nombre + apellido + ", ¿Cómo estás?";
 
-const saludo2 = `hola, ${nombre} ${apellido}  , ¿cómo estás?`; //usar una variable en un string se llama string interpolation
+console.log(saludo)
 
-// console.log(saludo2);
+const saludo2 = `hola, ${nombre} ${apellido} , ¿cómo estás?`; //usar una variable en un string se llama string interpolation
 
+console.log(saludo2);
 
 
 descripcionPersonal =`${nombre} ${apellido} es de ${ciudad}.`
 
 // console.log(descripcionPersonal)
 
-const datosPersonalesEnTresLineas = ` Me llamo ${nombre},
+const datosPersonalesEnTresLineas = `Me llamo ${nombre},
  mi primer apellido es ${apellido},
  y soy de ${ciudad}`
 
-//  
+console.log(datosPersonalesEnTresLineas)
